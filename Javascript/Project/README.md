34c4e18314b5b10960c4d1c149a76d96df6d838d
kevinbalderas01 <balderaskevin01@gmail.com>
2024-10-03 02:37:26 -0600

    ReadMe added and changes in html and js

---
+++ Javascript/Project/README.md
@@ -0,0 +1,21 @@
+# Projecto Javascript - 6000
+
+## Primera Fase
+
+En esta fase inicial, se utilizan variables, funciones, expresiones regulares para validar entrada por medio de promps y mostrar resultados en consola. A continuación, la pantalla inicial:
+![Pantalla inicial](./img/1.png)
+
+Pedimos datos como nombre, para este dato no validamos.
+![Pidiendo nombre](./img/2.png)
+
+Luego pedimos dato de edad, para lo cual validamos con simplemente viendo si es NaN luego de convertilo a número el string ingresado.
+![Pidiendo edad](./img/3.png)
+
+Luego pedimos el dato de país de origen, donde igual validamos con una expresion regular sencilla.
+
+ Primer Intento  | Segundo Intento 
+ ------------- | ------------- 
+ ![](./img/4.png)  | ![](./img/5.png)  
+
+ Por último, mostramos los datos resultantes de la ruta a cursar con simples condiciones básicos y mostramos en consola.
+ ![Pidiendo edad](./img/6.png)
---
+++ Javascript/Project/app.js
@@ -0,0 +1,67 @@
+//Funciones
+function validarEdad(edad) {
+    //Funcion que compara lo ingresado en edad en base a si es Nan 
+    if (!Number.isNaN(edad)){
+        console.log(`Edad de ${edad} ingresada correctamente.`)
+        return edad
+    }
+    return 0
+    
+}
+
+function validarCadenaSoloTexto(texto) {
+    //Utilice expresión regular que asegura tenga solo letras lo ingresado por usuario.
+    //Es decir, si existen numeros en la cadena, devuelve positivo, por lo que invertimos el resultado
+    const re = /[0-9]+/;
+    const resultado =re.test(texto)
+    if (!resultado) {
+        console.log(`Pais ${texto} ingresado correctamente.`)
+    }
+    return !resultado
+    
+}
+
+function regresarCarrera(numero){
+    if (numero === 1) {
+        console.log('Cursa HTML, CSS, JS Y VUE')
+        return true
+    } else if (numero === 2){
+        console.log('Cursa SQL, JS, PYTHON, NODE')
+        return true
+    } else if (numero === 3){
+        console.log('Cursa SQL, PYTHON, MATEMATICAS')
+        return true
+    } 
+    return false
+}
+
+//Logica prinpcial
+alert('Bienvenido a nuestro sitio de cursos!')
+alert('Vamos a hacerte unas preguntas primero !')
+
+const nombre = prompt('Cuál es tu nombre?')
+console.log(`Nombre ${nombre} ingresado correctamente.`)
+
+//Bucle para la entrada/validación de edad
+let edad;
+do {
+    edad = validarEdad(parseInt(prompt('Cuál es tu edad?')))
+} while (edad===0);
+
+//Bucle para verificar pais de origen
+let pais = validarCadenaSoloTexto(prompt('Cuál es tu país de origen?'))
+while (!pais) {
+    pais = validarCadenaSoloTexto(prompt('Cuál es tu país de origen?- Sólo letras'))
+
+}
+
+//Bucle para verificar entrada y devolver carrera
+let carrera = false
+while (!carrera) {
+    carrera = regresarCarrera(parseInt(prompt('Ingresa el número de la ruta interesada: 1- FrontEnd , 2-BackEnd, 3-Data Science')))
+}
+
+
+
+//Fase final, mostramos datos
+alert('Gracias por elegirnos!')
